191dbc02cc9560b671f05dd0bc5f5061d6459ba2
Luo <luojaa@ncbi.nlm.nih.gov>
2024-04-03 10:10:52 -0400

    modified scrapers to overcome two bugs:some knumbers aren't knumbers anymore (??) and some web pages have alternate formats, which broke the pattern matching code to get descriptors

--- KEGG/01.KOG_2_uids.py
+++ KEGG/01.KOG_2_uids.py
@@ -23,7 +23,10 @@ kog_ids = [k for k in kog_ids_raw if k not in not_kog_ids]
 def extract_uid_htmlrow(rowtext):
     uid = re.findall(r'">(.*)<\/a', rowtext)[0]
     return uid
-def extract_descriptor_htmlrow(rowtext):
+def extract_descriptor_htmlrow(rowtext, altformat = False):
+    if altformat:
+        descriptor = re.findall(r'[^\n].*$', descriptor_row)[0].split("\xa0")[-1]
+        return descriptor
     descriptor = re.findall(r';\s(.*)', rowtext)[0] 
     return descriptor
 
@@ -33,27 +36,39 @@ def get_uids(kogid): # also gets descriptor for KOG
     data = requests.get(url)
     print("completed request for {}".format(kogid))
     html = BeautifulSoup(data.text, 'html.parser')
+    try: 
+        descriptor_row = html.div.text
+    except:
+        with open("not_knumber_anymore.log", "a") as outfile:
+            print(kogid, file=outfile)
+            return
+    try:
+        descriptor = extract_descriptor_htmlrow(descriptor_row)
+    except: # row is formated without ";" separating the gene alias and descriptor
+        descriptor = extract_descriptor_htmlrow(descriptor_row, True)
+        
     for i in range(2, len(html.find_all("td")), 4):
         rowtext = str(html.find_all("td")[i])
         uid = extract_uid_htmlrow(rowtext)
         uids.append(uid)
-    descriptor_row = html.div.text
-    descriptor = extract_descriptor_htmlrow(descriptor_row)
     output = "data/kog_uid/{}.tsv".format(kogid)
     with open(output, "a") as outfile:
         for uid in uids:
             print("\t".join([kogid, uid, descriptor]), file=outfile)
 
 section, num_sections = int(sys.argv[1]), int(sys.argv[2])
-print(section)
-section_indices = [i for i in range(0, len(kog_ids), num_sections)]
+#print(section)
+section_indices = [i for i in range(0, len(kog_ids), len(kog_ids) // num_sections)]
 start_range = section_indices[section-1]
 if section == num_sections:
     end_range = len(kog_ids)
 else:
     end_range = section_indices[section]
+
+start_knumber = int(sys.argv[3])
     
 for kog_id in kog_ids[start_range:end_range]:
-    print(kog_id)
-    #get_uids(kog_id)
+   # print(kog_id)
+    if int(kog_id[1:]) >= start_knumber:
+        get_uids(kog_id)
 
